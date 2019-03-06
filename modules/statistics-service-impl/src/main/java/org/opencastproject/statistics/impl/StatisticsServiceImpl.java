d97ae692bd92033bf6acdb662dda71211e67c000
Kim Rinnewitz <krinnewitz@plapadoo.de>
2019-03-06 17:32:32 +0100

    Listen for changes on activemq

--- modules/statistics-service-impl/src/main/java/org/opencastproject/statistics/impl/StatisticsServiceImpl.java
+++ modules/statistics-service-impl/src/main/java/org/opencastproject/statistics/impl/StatisticsServiceImpl.java
@@ -21,6 +21,10 @@
 
 package org.opencastproject.statistics.impl;
 
+import org.opencastproject.message.broker.api.BaseMessage;
+import org.opencastproject.message.broker.api.MessageReceiver;
+import org.opencastproject.message.broker.api.MessageSender;
+import org.opencastproject.message.broker.api.assetmanager.AssetManagerItem;
 import org.opencastproject.statistics.api.StatisticsService;
 
 import org.influxdb.InfluxDB;
@@ -33,10 +37,15 @@ import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Serializable;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.List;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
 import java.util.stream.Collectors;
 
 /**
@@ -46,6 +55,8 @@ public class StatisticsServiceImpl implements StatisticsService, ManagedService 
 
   /** Logging utility */
   private static final Logger logger = LoggerFactory.getLogger(StatisticsServiceImpl.class);
+
+  private static final String QUEUE_ID = "ASSETMANAGER.Statistics";
 
   private static final String KEY_INFLUX_URI = "influx.uri";
   private static final String KEY_INFLUX_USER = "influx.username";
@@ -61,8 +72,26 @@ public class StatisticsServiceImpl implements StatisticsService, ManagedService 
 
   private InfluxDB influxDB;
 
+  private final ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+  private MessageReceiver messageReceiver;
+  private MessageWatcher messageWatcher;
+
+  public void setMessageReceiver(MessageReceiver messageReceiver) {
+    this.messageReceiver = messageReceiver;
+  }
+
   public void activate(ComponentContext cc) {
     logger.info("Activating Statistics Service");
+    messageWatcher = new MessageWatcher();
+    singleThreadExecutor.execute(messageWatcher);
+  }
+
+  public void deactivate(ComponentContext cc) {
+    logger.info("Deactivating Statistics Service");
+    if (messageWatcher != null) {
+      messageWatcher.stopListening();
+    }
+    singleThreadExecutor.shutdown();
   }
 
   @Override
@@ -180,6 +209,70 @@ public class StatisticsServiceImpl implements StatisticsService, ManagedService 
           .collect(Collectors.toList()));
     }
     return new Views(labels, values);
+  }
+
+  private class MessageWatcher implements Runnable {
+
+    private final Logger logger = LoggerFactory.getLogger(MessageWatcher.class);
+
+    private volatile boolean listening = true;
+    private FutureTask<Serializable> future;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    void stopListening() {
+      this.listening = false;
+      future.cancel(true);
+    }
+
+    @Override
+    public void run() {
+      logger.info("Starting to listen for episode update messages");
+      while (listening) {
+        future = messageReceiver.receiveSerializable(QUEUE_ID, MessageSender.DestinationType.Queue);
+        executor.execute(future);
+        try {
+          final BaseMessage baseMessage = (BaseMessage) future.get();
+          if (baseMessage.getObject() instanceof AssetManagerItem.DeleteEpisode) {
+            final AssetManagerItem.DeleteEpisode item = (AssetManagerItem.DeleteEpisode) baseMessage.getObject();
+            if (AssetManagerItem.Type.Delete.equals(item.getType())) {
+              // If an episode is deleted from opencast, also delete it from influxdb
+              final Query query = BoundParameterQuery.QueryBuilder.newQuery("DELETE FROM " + influxViewsMeasurement + " WHERE episodeId=$episodeId")
+                  .forDatabase(influxDbName)
+                  .bind("episodeId", item.getMediaPackageId())
+                  .create();
+              final QueryResult result = influxDB.query(query);
+              if (result.hasError()) {
+                 logger.warn("Couldn't remove statistics for episode {} reason: {}", item.getMediaPackageId(), result.getError());
+              }
+            }
+          }
+          if (baseMessage.getObject() instanceof AssetManagerItem.TakeSnapshot) {
+            final AssetManagerItem.TakeSnapshot item = (AssetManagerItem.TakeSnapshot) baseMessage.getObject();
+            if (AssetManagerItem.Type.Update.equals(item.getType())) {
+              // If an episode is moved to a different series, also update influxdb data
+              final String episodeId = item.getMediapackage().getIdentifier().toString();
+              final String seriesId = item.getMediapackage().getSeries();
+              final Query query = BoundParameterQuery.QueryBuilder.newQuery("SELECT COUNT(*) FROM " + influxViewsMeasurement + " WHERE episodeId=$episodeId AND seriesId!=$seriesId")
+                  .forDatabase(influxDbName)
+                  .bind("episodeId", episodeId)
+                  .bind("seriesId", seriesId)
+                  .create();
+              final boolean needsUpdate = influxDB.query(query).getResults().stream().anyMatch(r -> r.getSeries() != null);
+              if (needsUpdate) {
+                logger.info("Series for episode with id {} needs to be updated in influxdb", episodeId);
+                // TODO: Handle series change
+              }
+            }
+          }
+        } catch (CancellationException e) {
+          logger.trace("Listening for episode update/delete messages has been cancelled.");
+        } catch (Throwable t) {
+          logger.error("Problem while getting episode update/delete message events {}", t);
+        }
+      }
+      logger.info("Stopping listening for episode update messages");
+    }
+
   }
 
 }
