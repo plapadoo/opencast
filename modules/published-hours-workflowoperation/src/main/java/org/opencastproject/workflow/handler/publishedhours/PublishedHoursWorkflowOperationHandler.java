0f0d2b106355d5b85184bce72775e8130304d8b2
Philipp Middendorf <middendorf@plapadoo.de>
2019-04-30 15:51:34 +0200

    initial commit

---
+++ modules/published-hours-workflowoperation/src/main/java/org/opencastproject/workflow/handler/publishedhours/PublishedHoursWorkflowOperationHandler.java
@@ -0,0 +1,66 @@
+/**
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ *
+ * The Apereo Foundation licenses this file to you under the Educational
+ * Community License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at:
+ *
+ *   http://opensource.org/licenses/ecl2.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
+package org.opencastproject.workflow.handler.publishedhours;
+
+import org.opencastproject.job.api.JobContext;
+import org.opencastproject.mediapackage.MediaPackage;
+import org.opencastproject.mediapackage.MediaPackageElementFlavor;
+import org.opencastproject.mediapackage.Track;
+import org.opencastproject.workflow.api.AbstractWorkflowOperationHandler;
+import org.opencastproject.workflow.api.WorkflowInstance;
+import org.opencastproject.workflow.api.WorkflowOperationException;
+import org.opencastproject.workflow.api.WorkflowOperationInstance;
+import org.opencastproject.workflow.api.WorkflowOperationResult;
+import org.opencastproject.workflow.api.WorkflowOperationResult.Action;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PublishedHoursWorkflowOperationHandler extends AbstractWorkflowOperationHandler {
+
+  private static final Logger logger = LoggerFactory.getLogger(PublishedHoursWorkflowOperationHandler.class);
+
+  /** Flavorfilter */
+  private static final String OPT_FLAVOR = "flavor";
+
+  /**
+   * {@inheritDoc}
+   *
+   * @see org.opencastproject.workflow.api.WorkflowOperationHandler#start(org.opencastproject.workflow.api.WorkflowInstance,
+   *      JobContext)
+   */
+  @Override
+  public WorkflowOperationResult start(WorkflowInstance workflowInstance, JobContext context)
+          throws WorkflowOperationException {
+    final MediaPackage mediaPackage = workflowInstance.getMediaPackage();
+
+    final WorkflowOperationInstance operation = workflowInstance.getCurrentOperation();
+    final String flavor = operation.getConfiguration(OPT_FLAVOR);
+
+    for (Track track : mediaPackage.getTracks(MediaPackageElementFlavor.parseFlavor(flavor))) {
+
+    }
+
+    return createResult(mediaPackage, Action.CONTINUE);
+    //return createResult(mediaPackage, Action.CONTINUE, totalTimeInQueue);
+  }
+}
