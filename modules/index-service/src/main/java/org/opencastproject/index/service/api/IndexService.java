7b2d3149fe6fc0dbd913a53cb22bb3e83060f1ef
Kim Rinnewitz <krinnewitz@plapadoo.de>
2019-05-09 10:30:25 +0200

    Improve handling of NotFoundExceptions

--- modules/index-service/src/main/java/org/opencastproject/index/service/api/IndexService.java
+++ modules/index-service/src/main/java/org/opencastproject/index/service/api/IndexService.java
@@ -43,6 +43,7 @@ import org.opencastproject.security.api.UnauthorizedException;
 import org.opencastproject.series.api.SeriesException;
 import org.opencastproject.userdirectory.ConflictException;
 import org.opencastproject.util.NotFoundException;
+import org.opencastproject.workflow.api.WorkflowDatabaseException;
 import org.opencastproject.workflow.api.WorkflowException;
 import org.opencastproject.workflow.api.WorkflowInstance;
 
@@ -67,7 +68,7 @@ public interface IndexService {
   }
 
   enum EventRemovalResult {
-    SUCCESS, FAILED, RETRACTING
+    SUCCESS, GENERAL_FAILURE, NOT_FOUND, RETRACTING
   }
 
   SearchResult<Group> getGroups(String filter, Opt<Integer> limit, Opt<Integer> offset, Opt<String> sort,
@@ -228,8 +229,13 @@ public interface IndexService {
    * @return A result which tells if the event was removed, removal failed, or the event is being retracted and will be removed later.
    * @throws UnauthorizedException
    *           Thrown if the action is unauthorized
+   * @throws WorkflowDatabaseException
+   *           Thrown if the workflow database is not reachable. This may be a temporary problem.
+   * @throws NotFoundException
+   *           If the configured retract workflow cannot be found. This is most likely a configuration issue.
    */
-  EventRemovalResult removeEvent(Event event, Runnable doOnNotFound, String retractWorkflowId) throws UnauthorizedException;
+  EventRemovalResult removeEvent(Event event, Runnable doOnNotFound, String retractWorkflowId)
+      throws UnauthorizedException, WorkflowDatabaseException, NotFoundException;
 
   /**
    * Removes an event.
